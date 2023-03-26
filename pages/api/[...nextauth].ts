7a37de8b973479de6b2541d6689681714ae44ac9
lucas.anacleto <lucas.anacleto@outlook.com>
2023-03-25 23:59:59 -0300

    add config mongo db

---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,58 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { compare } from 'bcrypt';
+
+import prismadb from '@/lib/prismadb';
+
+export default NextAuth({
+    providers: [
+        Credentials({
+            id: 'credentials',
+            name: 'Credentials',
+            credentials: {
+                email: {
+                    label: 'Email',
+                    type: 'text',
+                },
+                password: {
+                    label: 'Password',
+                    type: 'password',
+                }
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error('Email e senha obrigatório');
+                }
+
+                const user = await prismadb.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                });
+
+                if (!user || !user.hashedPassaword) {
+                    throw new Error('Email não existe');
+                }
+
+                const isCorrectPassword = await compare(credentials.password, user.hashedPassaword);
+
+                if (!isCorrectPassword) {
+                    throw new Error('Senha incorreta');
+                }
+
+                return user;
+            }
+        })
+    ],
+    pages: {
+        signIn: '/auth',
+    },
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: 'jwt',
+    },
+    jwt: {
+        secret: process.env.NEXTAUTH_JWT_SECRET,
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+});
