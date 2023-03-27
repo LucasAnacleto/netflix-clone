ea058bc209bb55b7401de04d4ab1a52b240b1ed6
lucas.anacleto <lucas.anacleto@outlook.com>
2023-03-27 02:26:36 -0300

    ajuste

---
+++ hooks/useMovieList.ts
@@ -0,0 +1,18 @@
+import userSWR from 'swr'
+import fetcher from '@/lib/fetcher'
+
+const useMovieList = () => {
+    const { data, error, isLoading} = userSWR('/api/movies', fetcher, {
+        revalidateIfStale: false,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+    });
+
+    return {
+        data,
+        error,
+        isLoading
+    }
+};
+
+export default useMovieList;
